73492ebd9b91c7f405d2b0250429604879bb0321
codeiwan <pewlv7976@gmail.com>
2025-01-20 00:46:49 +0900

    feat: 템플릿에서 데이터 렌더링: - 멜론 Top 100 데이터 렌더링 - html 코드 스타일 변경

--- hottrack/views.py
+++ hottrack/views.py
@@ -1,6 +1,24 @@
+import json
+from urllib.request import urlopen
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from hottrack.models import Song
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request=request, template_name="hottrack/index.html", context={})
+    melon_chart_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230910.json"
+    json_string = urlopen(melon_chart_url).read().decode("utf-8")
+    song_list = [
+        Song.from_dict(song_dict)
+        for song_dict in json.loads(json_string)
+    ]
+
+    return render(
+        request=request,
+        template_name="hottrack/index.html",
+        context={
+            "song_list": song_list,
+        },
+    )
