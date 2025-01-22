bbe3b0587322cd6c9b3f953d9094cb76da20986a
codeiwan <pewlv7976@gmail.com>
2025-01-22 23:08:18 +0900

    feat: 검색 기능 구현 - 검색어 입력창 구현 - 검색어 전달 및 검색결과 필터링

--- hottrack/views.py
+++ hottrack/views.py
@@ -8,6 +8,8 @@ from hottrack.models import Song
 
 
 def index(request: HttpRequest) -> HttpResponse:
+    query = request.GET.get("query", "").strip()
+
     melon_chart_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230910.json"
     json_string = urlopen(melon_chart_url).read().decode("utf-8")
     song_list = [
@@ -15,10 +17,20 @@ def index(request: HttpRequest) -> HttpResponse:
         for song_dict in json.loads(json_string)
     ]
 
+    if query:
+        song_list = [
+            song
+            for song in song_list
+            if query in song.name
+            or query in song.artist_name
+            or query in song.album_name
+        ]
+
     return render(
         request=request,
         template_name="hottrack/index.html",
         context={
             "song_list": song_list,
+            "query": query,
         },
     )
