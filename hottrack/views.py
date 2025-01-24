6cbc1f65d3de43f2c284cb920371d30eb1832816
codeiwan <pewlv7976@gmail.com>
2025-01-24 23:23:08 +0900

    feat: 장고 모델과 데이터베이스 활용 - 모델 생성 및 마이그레이션 - 커스텀 관리 명령을 통한 데이터베이스 저장 - View에서 모델 활용

--- hottrack/views.py
+++ hottrack/views.py
@@ -1,6 +1,7 @@
 import json
 from urllib.request import urlopen
 
+from django.db.models import QuerySet, Q
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
@@ -10,27 +11,20 @@ from hottrack.models import Song
 def index(request: HttpRequest) -> HttpResponse:
     query = request.GET.get("query", "").strip()
 
-    melon_chart_url = "https://raw.githubusercontent.com/pyhub-kr/dump-data/main/melon/melon-20230910.json"
-    json_string = urlopen(melon_chart_url).read().decode("utf-8")
-    song_list = [
-        Song.from_dict(song_dict)
-        for song_dict in json.loads(json_string)
-    ]
+    song_qs: QuerySet[Song] = Song.objects.all()
 
     if query:
-        song_list = [
-            song
-            for song in song_list
-            if query in song.name
-            or query in song.artist_name
-            or query in song.album_name
-        ]
+        song_qs = song_qs.filter(
+            Q(name__icontains=query)
+            | Q(artist_name__icontains=query)
+            | Q(album_name__icontains=query)
+        )
 
     return render(
         request=request,
         template_name="hottrack/index.html",
         context={
-            "song_list": song_list,
+            "song_list": song_qs,
             "query": query,
         },
     )
