dca0dd9f6350603e8a9988a3f5fa5fa6878271e5
codeiwan <pewlv7976@gmail.com>
2025-01-26 06:04:17 +0900

    feat: admin 앱을 통한 Song 데이터 관리 - admin에 Song 모델 등록 - 좋아요 수 갱신 액션 추가

--- hottrack/admin.py
+++ hottrack/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Song
+from .utils.melon import get_likes_dict
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    search_fields = ["name", "artist_name", "album_name"]
+    list_display = [
+        "cover_image",
+        "name",
+        "artist_name",
+        "album_name",
+        "genre",
+        "like_count",
+        "release_date",
+    ]
+    list_filter = ["genre", "release_date"]
+    actions = ["update_like_count"]
+
+    @staticmethod
+    def cover_image(song) -> str:
+        return format_html('<img src="{}" style="width: 50px;" />', song.cover_url)
+
+    def update_like_count(self, request, queryset):
+        melon_uid_list = queryset.values_list("melon_uid", flat=True) 
+        likes_dict = get_likes_dict(melon_uid_list)
+        print("likes_dict :", likes_dict)
+
+        # 일괄 UPDATE 쿼리
+        changed_count = 0
+        for song in queryset:
+            if song.like_count != likes_dict[song.melon_uid]:
+                song.like_count = likes_dict[song.melon_uid]
+                changed_count += 1
+
+        Song.objects.bulk_update(
+            queryset,
+            fields=["like_count"],
+        )
+
+        self.message_user(request, f"{changed_count} 곡의 좋아요 갱신 완료")
